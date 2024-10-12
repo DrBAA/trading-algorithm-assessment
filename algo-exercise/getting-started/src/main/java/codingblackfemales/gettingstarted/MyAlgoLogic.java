c72312ca402097a4402a0991c86beb59dcea6508
Beth Austin <muchiribeth@yahoo.co.uk>
2024-10-12 21:39:01 +0100

    amended MyAlgoTest class. Made my variables final. BUILD SUCCESS. code now looks FINAL

--- algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
+++ algo-exercise/getting-started/src/main/java/codingblackfemales/gettingstarted/MyAlgoLogic.java
@@ -14,10 +14,14 @@ import messages.order.Side;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-// 8TH AND FINAL CODE - GOOD CODE
-// CODE TO CREATE AND CANCEL ORDERS BASED ON THE SPREAD NARROWING AND EXPANDING
-// IF THE SPREAD NARROWS TO OR BELOW 5 POINTS CREATE 5 ORDERS FOR 200 SHARES PER ORDER,
-// IF THE SPREAD WIDENS TO OR ABOVE CERTAIN POINTS, CANCEL ANY UNFILLED OR PARTIALLY FILLED ORDER
+// 8TH AND FINAL CODE
+// THIS ALGO CREATES AND CANCELS CHILD ORDERS BASED ON THE SPREAD NARROWING AND EXPANDING
+// IF THE SPREAD NARROWS TO OR BELOW 5 POINTS, CREATE 5 ORDERS FOR 200 SHARES PER ORDER,
+// FOR EACH UNFILLED/PARTIALLY FILLED ORDER, IF THE BEST ASK PRICE IS ABOVE THE BUY PRICE FOR THAT
+// PLUS 7 POINTS, CANCEL THAT  ORDER
+// RESULTS - BASED ON 9 MARKET DATA TICKS, THE ALGO CREATED 8 BUY ORDERS, GOT 2 FULLY FILLED ORDERS,
+// 1 PARTIAL FILLED, CANCELLED 5 ORDERS AND HAS 3 ACTIVE ORDERS REMAINING (1 NOT FILLED)
+
 
 public class MyAlgoLogic implements AlgoLogic {
     private static final Logger logger = LoggerFactory.getLogger(MyAlgoLogic.class);
@@ -25,15 +29,15 @@ public class MyAlgoLogic implements AlgoLogic {
     @Override
     public Action evaluate(SimpleAlgoState state) {
 
-        var orderBookAsString = Util.orderBookToString(state);
+        final var orderBookAsString = Util.orderBookToString(state);
 
         logger.info("[MYALGO] The state of the order book is:\n" + orderBookAsString);
 
         /** Algo **/
 
         // Get the best bid and best ask levels     
-        BidLevel bestBid = state.getBidAt(0);
-        AskLevel bestAsk = state.getAskAt(0);
+        final BidLevel bestBid = state.getBidAt(0);
+        final AskLevel bestAsk = state.getAskAt(0);
 
         // Get the best bid and ask prices and their corresponding quantities
         final long bestBidPrice = bestBid.getPrice();
@@ -43,22 +47,15 @@ public class MyAlgoLogic implements AlgoLogic {
         final long buyQuantity = 200L;
 
         // Define a threshold for the spread (in price points)
-        long spread = Math.abs(bestAskPrice - bestBidPrice); // added 5/10/2024 to get absolute value of a number
-        long spreadThreshold = 5L;
+        final long spread = Math.abs(bestAskPrice - bestBidPrice); // added 5/10/2024 to get absolute value of a number
+        final long spreadThreshold = 5L;
 
         // Log the bid-ask information
         logger.info("[MYALGO] Best bid: " + bestBidQuantity + " @ " + bestBidPrice + " Best ask: " + bestAskQuantity + " @ " + bestAskPrice);
-
-        // returns all orders regardless of their status (ie active, cancelled, filled, modified, etc)
-        // var totalOrderCount = state.getChildOrders().size();    
-
-        // if (totalOrderCount > 20) { // original code            
-        //     return NoAction.NoAction;
-        // }
-    
+   
         
         // Retrieve the list of active child orders
-        var activeChildOrders = state.getActiveChildOrders();
+        final var activeChildOrders = state.getActiveChildOrders();
 
 
         // TO CREATE CHILD ORDERS
@@ -82,7 +79,7 @@ public class MyAlgoLogic implements AlgoLogic {
         }      
         else {
             // If there are already 5 active child orders, log it and take no action
-            logger.info("BUY CONDITIONS - [MYALGO] has " + activeChildOrders.size() + " active child orders. No new orders will be created.");
+            logger.info("BUY CONDITIONS - [MYALGO] has " + activeChildOrders.size() + " active child orders. No new orders created.");
         }             
         
 
@@ -91,7 +88,7 @@ public class MyAlgoLogic implements AlgoLogic {
         long orderId;
         Side orderSide;        
         long orderQuantity;        
-        long orderPrice;
+        long eachOrderPrice;
         long orderFilledQuantity;
         long unfilledQuantity;
 
@@ -107,7 +104,7 @@ public class MyAlgoLogic implements AlgoLogic {
                     orderId = order.getOrderId();
                     orderSide = order.getSide();
                     orderQuantity = order.getQuantity();                    
-                    orderPrice = order.getPrice();
+                    eachOrderPrice = order.getPrice();
                     orderFilledQuantity = order.getFilledQuantity();
 
                         // Check if the order is partially filled or not filled at all
@@ -121,12 +118,13 @@ public class MyAlgoLogic implements AlgoLogic {
                                             " Order ID: " + orderId +
                                             " Side: " + orderSide +
                                             ", Ordered Qty: " + orderQuantity +
-                                            ", Price: " + orderPrice +                                            
+                                            ", Price: " + eachOrderPrice +                                            
                                             ", Filled Qty: " + orderFilledQuantity  +
                                             ", Unfilled Qty: " + unfilledQuantity);
 
                                 // Cancel the unfilled part of the order if the ASK price moves up by or above the defined threshold
-                                if (bestAskPrice >= (orderPrice + priceReversalThreshold)) {
+                                if (bestAskPrice >= (eachOrderPrice + priceReversalThreshold)) {
+                                    logger.info("[MYALGO] CANCEL CONDITIONS - BesAsk is: " + bestAsk + " OrderPrice: " + eachOrderPrice);
                                     logger.info("[MYALGO] CANCEL CONDITIONS - price reversal threshold is " + priceReversalThreshold + " points.");
                                     logger.info("[MYALGO] CANCEL CONDITIONS - Ask price moved against buy order. " +
                                                 " Cancelling order ID: " + orderId +
@@ -135,7 +133,7 @@ public class MyAlgoLogic implements AlgoLogic {
                                 }
                                 else {
                                     // If the ask price has not moved beyond the threshold, log that the order remains active
-                                    logger.info("[MYALGO] CANCEL CONDITIONS  - Ask price has not moved above threshold. Buy order to remain active");
+                                    logger.info("[MYALGO] CANCEL CONDITIONS  - Ask price is stil under threshold. Buy order to remain active");
                                 }
                         }
                         else {
@@ -152,12 +150,13 @@ public class MyAlgoLogic implements AlgoLogic {
 
         }  
         
-        // If no active child order is present, the method returns a NoAction object
+        // If no active child orders present, the method returns a NoAction object
         return NoAction.NoAction;
     
     }
 
 }
+
 
 
 
